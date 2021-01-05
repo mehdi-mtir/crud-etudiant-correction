614dd74cf86c475c9503bd36da00b4822c5cd974
Mehdi <mehdi.mtir@gmail.com>
2021-01-05 15:22:00 +0100

    version avec ajout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './model/student';
+import { PrixPipe } from './prix.pipe';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
+import { PrixPipe } from './prix.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
-    EditComponent
+    EditComponent,
+    PrixPipe
   ],
   imports: [
     BrowserModule
